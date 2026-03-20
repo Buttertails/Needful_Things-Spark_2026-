3385bea4051f7d7a4354cf8d331677cd8af8207a
Forrest <35341511+forrest-fishel@users.noreply.github.com>
2026-03-20 11:00:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,46 @@
-from flask import Flask, redirect, request, make_response
+from flask import Flask, redirect, url_for, session
 from authlib.integrations.flask_client import OAuth
 import os
 
-CLOUDFRONT_URL = 'https://d84l1y8p4kdic.cloudfront.net'
-
 app = Flask(__name__)
-app.secret_key = os.environ.get('SECRET_KEY', os.urandom(24))
+app.secret_key = os.urandom(24)  # Use a secure random key in production
 oauth = OAuth(app)
 
 oauth.register(
-    name='oidc',
-    authority='https://cognito-idp.us-east-1.amazonaws.com/us-east-1_SVWFWvH1I',
-    client_id='7l2fh3lmk09rfi1neoh3ghujek',
-    client_secret=os.environ.get('COGNITO_CLIENT_SECRET'),
-    server_metadata_url='https://cognito-idp.us-east-1.amazonaws.com/us-east-1_SVWFWvH1I/.well-known/openid-configuration',
-    client_kwargs={'scope': 'phone openid email'}
+  name='oidc',
+  authority='https://cognito-idp.us-east-1.amazonaws.com/us-east-1_kowqhZ4fl',
+  client_id='25is9h8u5rka8qi4sti9qnu0d2',
+  client_secret='<client secret>',
+  server_metadata_url='https://cognito-idp.us-east-1.amazonaws.com/us-east-1_kowqhZ4fl/.well-known/openid-configuration',
+  client_kwargs={'scope': 'email openid phone'}
 )
+
+@app.route('/')
+def index():
+    user = session.get('user')
+    if user:
+        return  f'Hello, {user["email"]}. <a href="/logout">Logout</a>'
+    else:
+        return f'Welcome! Please <a href="/login">Login</a>.'
 
 @app.route('/login')
 def login():
-    redirect_uri = request.host_url.rstrip('/') + '/authorize'
-    return oauth.oidc.authorize_redirect(redirect_uri)
+    # Alternate option to redirect to /authorize
+    # redirect_uri = url_for('authorize', _external=True)
+    # return oauth.oidc.authorize_redirect(redirect_uri)
+    return oauth.oidc.authorize_redirect('https://d84l1y8p4kdic.cloudfront.net')
 
 @app.route('/authorize')
 def authorize():
     token = oauth.oidc.authorize_access_token()
-    id_token = token.get('id_token', '')
-    # Pass token via secure cookie, redirect to static role selection page on CloudFront
-    resp = make_response(redirect(f'{CLOUDFRONT_URL}/role.html'))
-    resp.set_cookie('id_token', id_token, secure=True, samesite='Lax')
-    return resp
+    user = token['userinfo']
+    session['user'] = user
+    return redirect(url_for('index'))
 
 @app.route('/logout')
 def logout():
-    resp = make_response(redirect(CLOUDFRONT_URL))
-    resp.delete_cookie('id_token')
-    return resp
+    session.pop('user', None)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
